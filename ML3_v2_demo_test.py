e5ee38d542b7edbd80a4646d796da69a51c5d163
Chalkina <93142671+Chalkina@users.noreply.github.com>
2022-01-17 20:00:36 +0300

    Add files via upload

---
+++ ML3_v2_demo_test.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import GradientBoostingRegressor
+df = pd.read_csv('/Users/Roman/Downloads/housing.csv')
+if st.button('Отобразить первые пять строк'):
+    st.write(df.head(5))
+if st.button('Обучить модель'):
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('MEDV', axis=1),
+                                                        df['MEDV'],
+                                                        test_size=0.2,
+                                                        random_state=2100)
+    st.write('Разделили данные и передали в обучение')
+    regr_model = XGBRegressor()
+    regr_model.fit(X_train, y_train)
+    pred = regr_model.predict(X_test)
+    st.write('Обучили модель, MAE = ' + str(mean_absolute_error(y_test, pred)))
+test = 10, 15, 20, 25, 30, 35
+if st.selectbox(
+     'Какой размер test выборки?',
+     options=test):
+     st.write(pd.DataFrame({
+        'y_test',
+        'pred'}))
